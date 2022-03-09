c89a1b4aa748ce5d1b94651a4fb14849afc4ec7b
seth <seth623@gmail.com>
2022-03-08 17:14:33 -0700

    Completed Assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML5(html)
+
+programs = doc.css(".btn.btn-primary.btn-red") 
+programs.each do |program|
+    puts program.text.strip
+end  
